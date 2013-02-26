ec328de2ce1c1adf790703bbb7962241e9e19a76
Mike Williams <cmwilliams@gmail.com>
2013-02-26 17:17:07 -0500

    Added UI files

--- DaysUntoWeb/App_Start/BundleConfig.cs
+++ DaysUntoWeb/App_Start/BundleConfig.cs
@@ -8,36 +8,31 @@ namespace DaysUntoWeb
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/scripts").Include(
+                        "~/Scripts/jquery.js",
+                        "~/Scripts/bootstrap-transition.js",
+                        "~/Scripts/bootstrap-alert.js",
+                        "~/Scripts/bootstrap-modal.js",
+                        "~/Scripts//bootstrap-dropdown.js",
+                        "~/Scripts/bootstrap-scrollspy.js",
+                        "~/Scripts/bootstrap-tab.js",
+                        "~/Scripts/bootstrap-tooltip.js",
+                        "~/Scripts/bootstrap-popover.js",
+                        "~/Scripts/bootstrap-button.js",
+                        "~/Scripts/bootstrap-collapse.js",
+                        "~/Scripts/bootstrap-carousel.js",
+                        "~/Scripts/bootstrap-typeahead.js",
+                        "~/Scripts/application.js",
+                        "~/Scripts/jquery.lightbox-0.5.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+         
+            bundles.Add(new StyleBundle("~/css").Include(
+                "~/Content/bootstrap-2.css",
+                "~/Content/font-awesome.css",
+                "~/Content/bootstrap-responsive.css",
+                "~/Content/jquery.lightbox-0.5.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*",
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-
-            bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-                        "~/Content/themes/base/jquery.ui.core.css",
-                        "~/Content/themes/base/jquery.ui.resizable.css",
-                        "~/Content/themes/base/jquery.ui.selectable.css",
-                        "~/Content/themes/base/jquery.ui.accordion.css",
-                        "~/Content/themes/base/jquery.ui.autocomplete.css",
-                        "~/Content/themes/base/jquery.ui.button.css",
-                        "~/Content/themes/base/jquery.ui.dialog.css",
-                        "~/Content/themes/base/jquery.ui.slider.css",
-                        "~/Content/themes/base/jquery.ui.tabs.css",
-                        "~/Content/themes/base/jquery.ui.datepicker.css",
-                        "~/Content/themes/base/jquery.ui.progressbar.css",
-                        "~/Content/themes/base/jquery.ui.theme.css"));
+           
         }
     }
 }
