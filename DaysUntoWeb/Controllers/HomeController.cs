669c90943247d7459e7d65dae448aace4aca1368
Mike Williams <cmwilliams@gmail.com>
2013-02-28 17:09:32 -0500

    More layout changes

--- DaysUntoWeb/Controllers/HomeController.cs
+++ DaysUntoWeb/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -50,6 +51,41 @@ namespace DaysUntoWeb.Controllers
 
                 return View(model);
            
+        }
+
+
+        [HttpPost]
+        public ActionResult ImportCalendar(HttpPostedFileBase calendarfile)
+        {
+            if (calendarfile != null && calendarfile.ContentLength > 0)
+            {
+                var calendars = iCalendar.LoadFromStream(calendarfile.InputStream);
+                var occurrences = calendars.GetOccurrences(new iCalDateTime(DateTime.Now.Year, 1, 1),
+                                                           new iCalDateTime(2020, 12, 31));
+                foreach (var occurrence in occurrences)
+                {
+                    var rc = occurrence.Source as IRecurringComponent;
+                    if (rc != null)
+                    {
+                        var user = _context.UserProfiles.SingleOrDefault(u => u.UserId == WebSecurity.CurrentUserId);
+                        if (user != null)
+                        {
+                            var existingEvent = user.CalendarEvents.SingleOrDefault(c => c.CalendarEventDate == occurrence.Period.StartTime.Local && c.Name == rc.Summary);
+
+                            if (existingEvent == null)
+                            {
+                                user.CalendarEvents.Add(new CalendarEvent { CalendarEventDate = occurrence.Period.StartTime.Local, Name = rc.Summary});
+                            }
+                        }
+
+                        _context.SaveChanges();
+
+
+                    }
+                }
+            }
+            // redirect back to the index action to show the form once again
+            return RedirectToAction("Index");        
         }
 
         public ActionResult About()
