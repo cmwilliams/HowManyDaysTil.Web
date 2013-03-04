70e54c81927cb3b00ab7896be2e197e53cc1258a
Mike Williams <cmwilliams@gmail.com>
2013-03-04 09:24:58 -0500

    Made some name changes

--- DaysUntoWeb/App_Start/AuthConfig.cs
+++ DaysUntoWeb/App_Start/AuthConfig.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Web.Configuration;
+﻿using System.Web.Configuration;
 using Microsoft.Web.WebPages.OAuth;
-using DaysUntoWeb.Models;
 
 namespace DaysUntoWeb
 {
@@ -20,8 +15,8 @@ namespace DaysUntoWeb
             //    clientSecret: "");
 
             OAuthWebSecurity.RegisterTwitterClient(
-                consumerKey: "AbSUbjiiZ3H7er0qN8t0w",
-                consumerSecret: "4eqLlX4ItOPAaU4G1gAMSLDJlzUubMVXt7MKZ3kvoaY");
+                consumerKey: "2SB9Hbagjcuw1Uz8t0LVg",
+                consumerSecret: "vTxhylaCDKuU9Xmm68YW4F7ls7MR6gCQKmIjamM");
 
             OAuthWebSecurity.RegisterFacebookClient(
                 appId: WebConfigurationManager.AppSettings["FacebookAppId"],
